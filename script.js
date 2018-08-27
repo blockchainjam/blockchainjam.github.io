9f1aecca1b7310011aa3e8131a24122823e87aee
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-08-27 22:05:12 +0900

    [update]

--- script.js
+++ script.js
@@ -5,15 +5,13 @@ function scrollToSelector(selector) {
 $(function() {
     var $win = $(window),
         $nav = $('.navigation-bar'),
-        position = $('.outline').offset().top,
         fixedClass = 'floating';
   
     $win.on('load scroll', function() {
-      var value = $(this).scrollTop();
-      if ( value > position ) {
-        $nav.addClass(fixedClass);
+      if(value > $('.abstract').offset().top) {
+          $nav.addClass(fixedClass);
       } else {
-        $nav.removeClass(fixedClass);
+          $nav.removeClass(fixedClass);
       }
     });
   });
