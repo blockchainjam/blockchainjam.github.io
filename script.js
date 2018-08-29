234c6cdbaae66466ef79775d76dd994913c60110
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-08-29 12:34:42 +0900

    [update:

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function scrollToSelector(selector) {
-    $("html,body").animate({scrollTop:$(selector).offset().top});
+    $("html,body").animate({ scrollTop: $(selector).offset().top });
 }
 
-$(function() {
+$(function () {
     var $win = $(window),
-        $nav = $('.navigation-bar'),
-        fixedClass = 'floating';
-  
-    $win.on('load scroll', function() {
-      if(value > $('.abstract').offset().top) {
-          $nav.addClass(fixedClass);
-      } else {
-          $nav.removeClass(fixedClass);
-      }
+        $nav = $('.navigation-bar:not(.absolute)');
+
+    $win.on('load scroll', function () {
+        var value = $(this).scrollTop();
+        if (value > $('.display-nav').offset().top) {
+            $nav.removeClass('hidden');
+        } else {
+            $nav.addClass('hidden');
+        }
     });
-  });
+});
