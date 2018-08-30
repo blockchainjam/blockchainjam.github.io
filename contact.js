9e8387d5bbf949815d20acf895e7218afc47b4f1
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-08-30 21:39:56 +0900

    [update]

--- contact.js
+++ contact.js
@@ -1,7 +1,11 @@
 function postContact() {
-    $.post();
+    let result = window.confirm("お問い合わせを送信しますか？");
+    if(!result) {
+        return;
+    }
+    $.post("https://script.google.com/macros/s/AKfycbxJrZ6e-WMnzsix2AsTcKXJJSB-myABQoI2-JyyrfbGdsBg9zcx/exec", $("#contact-form").serialize());
 }
 
 function postMediaContact() {
-    $.post();
+    $.post("https://script.google.com/macros/s/AKfycbxJrZ6e-WMnzsix2AsTcKXJJSB-myABQoI2-JyyrfbGdsBg9zcx/exec", $("#media-contact-form").serialize());
 }
