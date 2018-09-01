ef38032df789c32e87058758c80d1b86a8853f1a
Ryotaro Miyagwa <o_o_o_o_o1031@yahoo.co.jp>
2018-09-01 23:07:46 +0900

    mailtoの実装

--- contact.js
+++ contact.js
@@ -1,17 +1,28 @@
 function postContact() {
     let result = window.confirm("お問い合わせを送信しますか？");
-    if(!result) {
+    if (!result) {
         return;
     }
-    $.post("https://script.google.com/macros/s/AKfycbxJrZ6e-WMnzsix2AsTcKXJJSB-myABQoI2-JyyrfbGdsBg9zcx/exec", $("#contact-form").serialize());
+    var name = $("#name").val()
+    var email = $("#email").val()
+    var content = $("#content").val()
+    var url = `mailto:beny@blockchainjam.org?subject=BlockChainJamへのお問い合わせ\&body=名前：${name}%0D%0Aメール：${email}%0D%0A内容：${content}`
+
+    location.href = url
     location.href = "completed.html"
 }
 
 function postMediaContact() {
     let result = window.confirm("お問い合わせを送信しますか？");
-    if(!result) {
+    if (!result) {
         return;
     }
-    $.post("https://script.google.com/macros/s/AKfycbxTgXOUPq4t1t7O0prRTyYqJLirwdjO0_rp-FhpSwznaMVOGks/exec", $("#media-contact-form").serialize());
-    location.href = "completed.html"
+    var name = $("#name").val()
+    var company = $("#company").val()
+    var email = $("#email").val()
+    var phone = $("#phone").val()
+    var content = $("#content").val()
+    var url = `mailto:beny@blockchainjam.org?subject=BlockChainJamへのお問い合わせ\&body=名前：${name}%0D%0A会社名：${company}%0D%0Aメール：${email}%0D%0A電話番号：${phone}%0D%0A内容：${content}`
+    location.href = url
+    // location.href = "completed.html"
 }
