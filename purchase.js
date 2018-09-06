69b424ebbabd1ff6cb3909f8ee99a6422ce1bbb2
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-09-06 20:20:25 +0900

    [update]

--- purchase.js
+++ purchase.js
@@ -1,4 +1,54 @@
-function purchase() {
+async function purchase() {
+    if (!window.PaymentRequest) {
+        window.alert("PaymentRequestAPIに対応したブラウザをお使いください。")
+        return;
+    }
+    let supportedInstruments = [{
+        supportedMethods: ['basic-card'],
+        data: {
+            supportedNetworks: [
+                'visa',
+                'mastercard'
+            ]
+        }
+    }];
+
+    var general = Number($("#general").val());
+    var student = Number($("#student").val());
+    var price = 10000 * general + 3000 * student;
+
+    let details = {
+        displayItems: [
+            {
+                label: `一般: 10000x ${general}`,
+                amount: {
+                    currency: "JPY",
+                    value: (general * 10000).toString()
+                }
+            },
+            {
+                label: `学生: 3000x ${student}`,
+                amount: {
+                    currency: "JPY",
+                    value: (student * 3000).toString()
+                }
+            }
+        ],
+        total: {
+            label: "合計",
+            amount: {
+                currency: "JPY",
+                value: price.toString()
+            }
+        }
+    };
+
+    let request = new PaymentRequest(supportedInstruments, details, { requestShipping: false });
+
+    let result = await request.show();
+    if (!result) {
+        return;
+    }
 
     Payjp.setPublicKey("pk_test_9bd57d50c30ed9d2ec978af1");
     Payjp.createToken({
@@ -6,26 +56,24 @@ function purchase() {
         cvc: result.details.cardSecurityCode,
         exp_month: result.details.expiryMonth,
         exp_year: result.details.expiryYear
-    }, async (status, response) => {
-        try {
-            if (status == 200) {
-                await this.http.post(
-                    "https://us-central1-ticket-p2p.cloudfunctions.net/capacitySupplement",
-                    {
-                        userId: this.auth.auth.currentUser!.uid,
-                        eventId: this.id,
-                        capacity: capacity,
-                        token: response.id
-                    }
-                ).toPromise();
-            } else {
-                throw Error();
-            }
-        } catch {
-            window.alert("エラー");
+    }, (status, response) => {
+        if (status == 200) {
+            $.post(
+                "https://us-central1-blockchainjam.cloudfunctions.net/purchase",
+                {
+                    name: $("#name").val(),
+                    email: $("#email").val(),
+                    general: general,
+                    student: student,
+                    token: response.id
+                },
+                () => {
+                    result.complete("success");
+                    location.href = "purchase-completed.html";
+                }
+            );
 
-            return;
-        } finally {
         }
+        result.complete("fail");
     });
 }
