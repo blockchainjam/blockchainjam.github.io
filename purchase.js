e7c8bc8e1de21de4feb531dab5a25bbe8e72c074
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-09-09 23:39:46 +0900

    [update]

--- purchase.js
+++ purchase.js
@@ -1,7 +1,7 @@
 const generalLimit = 80;
 const studentLimit = 20;
 
-$(function() {
+$(function () {
     $.get(
         "https://us-central1-blockchainjam.cloudfunctions.net/status"
     ).done((data) => {
@@ -10,92 +10,112 @@ $(function() {
         $("#generalPurchased").html(data.general);
         $("#studentPurchased").html(data.student);
 
-        if(data.general >= generalLimit) {
+        if (data.general >= generalLimit) {
             $("#general").attr("readonly", "true");
         }
-        if(data.student >= studentLimit) {
+        if (data.student >= studentLimit) {
             $("#student").attr("readonly", "true");
         }
     })
 
     if (!window.PaymentRequest) {
-        $("#purchaseForm").hide();
         $("#ifPaymentInvalid").show();
+        $("#ccName").attr("required", "true");
+        $("#ccNumber").attr("required", "true");
+        $("#ccExpMonth").attr("required", "true");
+        $("#ccExpYaer").attr("required", "true");
+        $("#ccCsc").attr("required", "true");
     }
 })
 
 async function purchase() {
-    if (!window.PaymentRequest) {
-        return;
-    }
-    let supportedInstruments = [{
-        supportedMethods: ['basic-card'],
-        data: {
-            supportedNetworks: [
-                'visa',
-                'mastercard'
-            ]
-        }
-    }];
-
     var general = Number($("#general").val());
     var student = Number($("#student").val());
 
-    if(Number($("#generalPurchased").val()) + general > generalLimit) {
+    if (Number($("#generalPurchased").val()) + general > generalLimit) {
         window.alert("定員を超えています。")
         return;
     }
 
-    if(Number($("#studentPurchased").val()) + student > studentLimit) {
+    if (Number($("#studentPurchased").val()) + student > studentLimit) {
         window.alert("定員を超えています。")
         return;
     }
 
     var price = 10000 * general + 3000 * student;
 
-    let details = {
-        displayItems: [
-            {
-                label: `一般: 10000x ${general}`,
+    var arg, result;
+
+    if (window.PaymentRequest) {
+        let supportedInstruments = [{
+            supportedMethods: ['basic-card'],
+            data: {
+                supportedNetworks: [
+                    'visa',
+                    'mastercard'
+                ]
+            }
+        }];
+
+        let details = {
+            displayItems: [
+                {
+                    label: `一般: 10000x ${general}`,
+                    amount: {
+                        currency: "JPY",
+                        value: (general * 10000).toString()
+                    }
+                },
+                {
+                    label: `学生: 3000x ${student}`,
+                    amount: {
+                        currency: "JPY",
+                        value: (student * 3000).toString()
+                    }
+                }
+            ],
+            total: {
+                label: "合計",
                 amount: {
                     currency: "JPY",
-                    value: (general * 10000).toString()
-                }
-            },
-            {
-                label: `学生: 3000x ${student}`,
-                amount: {
-                    currency: "JPY",
-                    value: (student * 3000).toString()
+                    value: price.toString()
                 }
             }
-        ],
-        total: {
-            label: "合計",
-            amount: {
-                currency: "JPY",
-                value: price.toString()
-            }
+        };
+
+        let request = new PaymentRequest(supportedInstruments, details, { requestShipping: false });
+
+        result = await request.show();
+        if (!result) {
+            return;
         }
-    };
 
-    let request = new PaymentRequest(supportedInstruments, details, { requestShipping: false });
+        arg = {
+            number: result.details.cardNumber,
+            cvc: result.details.cardSecurityCode,
+            exp_month: result.details.expiryMonth,
+            exp_year: result.details.expiryYear
+        };console.log(arg)
+    } else {
+        if(!window.confirm(`購入しますか？合計${price}円`)) {
+            return;
+        }
 
-    let result = await request.show();
-    if (!result) {
-        return;
+
+        arg = {
+            number: $("#ccNumber").val(),
+            cvc: $("#ccCsc").val(),
+            exp_month: $("#ccExpMonth").val(),
+            exp_year: $("#ccExpYear").val()
+        };
     }
+
 
     var name = $("#name").val();
     var email = $("#email").val();
 
     Payjp.setPublicKey("pk_test_9bd57d50c30ed9d2ec978af1");
-    Payjp.createToken({
-        number: result.details.cardNumber,
-        cvc: result.details.cardSecurityCode,
-        exp_month: result.details.expiryMonth,
-        exp_year: result.details.expiryYear
-    }, (status, response) => {
+    Payjp.createToken(arg, (status, response) => {
         if (status == 200) {
             console.log(response.id);
             $.post(
@@ -108,16 +128,24 @@ async function purchase() {
                     token: response.id
                 }
             ).done(() => {
-                result.complete("success");
+                if (result) {
+                    result.complete("success");
+                }
                 location.href = "purchase-completed.html";
                 return;
             }).fail(() => {
-                result.complete("fail");
-                window.alert("エラーが発生しました。こちらからご連絡を差し上げますので少々お待ちください。")
+                if (result) {
+                    result.complete("fail");
+                }
+                window.alert("エラーが発生しました。運営までお問い合わせください")
                 return;
             });
         } else {
-            result.complete("fail");
+            if (result) {
+                result.complete("fail");
+            } else {
+                window.alert("エラーが発生しました。")
+            }
         }
     });
 }
