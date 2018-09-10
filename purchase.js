644df5201063d8a7a8a2389dc47ec5b0ccd95b36
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-09-10 18:10:48 +0900

    [update]

--- purchase.js
+++ purchase.js
@@ -54,123 +54,130 @@ function confirmEmail() {
 }
 
 async function purchase() {
-    var general = Number($("#general").val());
-    var student = Number($("#student").val());
+    $("#cover").addClass("show");
+    try {
+        var general = Number($("#general").val());
+        var student = Number($("#student").val());
 
-    if (Number($("#generalPurchased").val()) + general > generalLimit) {
-        window.alert("定員を超えています。")
-        return;
-    }
-
-    if (Number($("#studentPurchased").val()) + student > studentLimit) {
-        window.alert("定員を超えています。")
-        return;
-    }
-
-    var price = 9000 * general + 3000 * student;
-
-    var arg, result;
-
-    if (!isInvalid) {
-        let supportedInstruments = [{
-            supportedMethods: ['basic-card'],
-            data: {
-                supportedNetworks: [
-                    'visa',
-                    'mastercard'
-                ]
-            }
-        }];
-
-        let details = {
-            displayItems: [
-                {
-                    label: `一般: 10000x ${general}`,
-                    amount: {
-                        currency: "JPY",
-                        value: (general * 9000).toString()
-                    }
-                },
-                {
-                    label: `学生: 3000x ${student}`,
-                    amount: {
-                        currency: "JPY",
-                        value: (student * 3000).toString()
-                    }
-                }
-            ],
-            total: {
-                label: "合計",
-                amount: {
-                    currency: "JPY",
-                    value: price.toString()
-                }
-            }
-        };
-
-        let request = new PaymentRequest(supportedInstruments, details, { requestShipping: false });
-
-        result = await request.show();
-        if (!result) {
+        if (Number($("#generalPurchased").val()) + general > generalLimit) {
+            window.alert("定員を超えています。")
             return;
         }
 
-        arg = {
-            number: result.details.cardNumber,
-            cvc: result.details.cardSecurityCode,
-            exp_month: result.details.expiryMonth,
-            exp_year: result.details.expiryYear
-        }; console.log(arg)
-    } else {
-        if (!window.confirm(`購入しますか？合計${price}円`)) {
+        if (Number($("#studentPurchased").val()) + student > studentLimit) {
+            window.alert("定員を超えています。")
             return;
         }
 
+        var price = 9000 * general + 3000 * student;
 
-        arg = {
-            number: $("#ccNumber").val(),
-            cvc: $("#ccCsc").val(),
-            exp_month: $("#ccExpMonth").val(),
-            exp_year: $("#ccExpYear").val()
-        };
-    }
+        var arg, result;
 
-
-    var name = $("#name").val();
-    var email = $("#email").val();
-
-    Payjp.setPublicKey("pk_test_9bd57d50c30ed9d2ec978af1");
-    Payjp.createToken(arg, (status, response) => {
-        if (status == 200) {
-            console.log(response.id);
-            $.post(
-                "https://us-central1-blockchainjam.cloudfunctions.net/purchase",
-                {
-                    name: name,
-                    email: email,
-                    general: general,
-                    student: student,
-                    token: response.id
+        if (!isInvalid) {
+            let supportedInstruments = [{
+                supportedMethods: ['basic-card'],
+                data: {
+                    supportedNetworks: [
+                        'visa',
+                        'mastercard'
+                    ]
                 }
-            ).done(() => {
-                if (result) {
-                    result.complete("success");
+            }];
+
+            let details = {
+                displayItems: [
+                    {
+                        label: `一般: 10000x ${general}`,
+                        amount: {
+                            currency: "JPY",
+                            value: (general * 9000).toString()
+                        }
+                    },
+                    {
+                        label: `学生: 3000x ${student}`,
+                        amount: {
+                            currency: "JPY",
+                            value: (student * 3000).toString()
+                        }
+                    }
+                ],
+                total: {
+                    label: "合計",
+                    amount: {
+                        currency: "JPY",
+                        value: price.toString()
+                    }
                 }
-                location.href = "purchase-completed.html";
+            };
+
+            let request = new PaymentRequest(supportedInstruments, details, { requestShipping: false });
+
+            result = await request.show();
+            if (!result) {
                 return;
-            }).fail(() => {
+            }
+
+            arg = {
+                number: result.details.cardNumber,
+                cvc: result.details.cardSecurityCode,
+                exp_month: result.details.expiryMonth,
+                exp_year: result.details.expiryYear
+            }; console.log(arg)
+        } else {
+            if (!window.confirm(`購入しますか？合計${price}円`)) {
+                return;
+            }
+
+
+            arg = {
+                number: $("#ccNumber").val(),
+                cvc: $("#ccCsc").val(),
+                exp_month: $("#ccExpMonth").val(),
+                exp_year: $("#ccExpYear").val()
+            };
+        }
+
+
+        var name = $("#name").val();
+        var email = $("#email").val();
+
+        Payjp.setPublicKey("pk_test_9bd57d50c30ed9d2ec978af1");
+        Payjp.createToken(arg, (status, response) => {
+            if (status == 200) {
+                console.log(response.id);
+                $.post(
+                    "https://us-central1-blockchainjam.cloudfunctions.net/purchase",
+                    {
+                        name: name,
+                        email: email,
+                        general: general,
+                        student: student,
+                        token: response.id
+                    }
+                ).done(() => {
+                    if (result) {
+                        result.complete("success");
+                    }
+                    location.href = "purchase-completed.html";
+                    return;
+                }).fail(() => {
+                    if (result) {
+                        result.complete("fail");
+                    }
+                    window.alert("エラーが発生しました。運営までお問い合わせください")
+                    return;
+                });
+            } else {
                 if (result) {
                     result.complete("fail");
+                } else {
+                    window.alert("エラーが発生しました。")
                 }
-                window.alert("エラーが発生しました。運営までお問い合わせください")
-                return;
-            });
-        } else {
-            if (result) {
-                result.complete("fail");
-            } else {
-                window.alert("エラーが発生しました。")
             }
-        }
-    });
+        });
+    } catch (e) {
+        throw (e.message);
+    } finally {
+        $("#cover").removeClass("show");
+    }
 }
